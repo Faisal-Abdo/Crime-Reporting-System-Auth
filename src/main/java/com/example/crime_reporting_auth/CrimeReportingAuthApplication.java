d7c44e6399e7ba06595c5302087ff87dbee909f4
Faisal-Abdo <145707185+Faisal-Abdo@users.noreply.github.com>
2025-11-11 22:41:14 +0400

    Initial commit

---
+++ src/main/java/com/example/crime_reporting_auth/CrimeReportingAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.crime_reporting_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeReportingAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeReportingAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crime_reporting_auth/CrimeReportingAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crime_reporting_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeReportingAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
