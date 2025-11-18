a451d82906a73fd4f92061b9724ec77c06a04947
Faisal-Abdo <145707185+Faisal-Abdo@users.noreply.github.com>
2025-11-18 22:52:39 +0400

    Added two entities.

---
+++ src/main/java/com/example/crime_reporting_auth/model/User.java
@@ -0,0 +1,27 @@
+package com.example.crime_reporting_auth.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "crimeApp_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
